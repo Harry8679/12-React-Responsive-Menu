68934b0b63c65e378e34b41085322bd81f70bc71
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-18 11:29:15 +0100

    Refactoring

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,61 +8,39 @@ import { AiOutlineBars } from "react-icons/ai";
 const Navbar = () => {
     return(
         <nav className="container navbar">
-      <div className="logo">
-        <BsFillRocketTakeoffFill size={35} color='#fff' />
-        <p className="logo-text">
-          Harry.<span>com</span>
-        </p>
-      </div>
-      <menu>
-        <ul
-          className="nav-links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Features</a>
-          </li>
-          <li>
-            <a href="#">Download</a>
-          </li>
-          <li>
-            <a href="#">Subscribe</a>
-          </li>
-          <li className="nav-btn">
-            <a className="btn btn-orange" href="#">
-              Get Started
-            </a>
-          </li>
-        </ul>
-      </menu>
-      <div className="menu-icons">
-        <AiOutlineBars color='#fff' size={27} />
-      </div>
-    </nav>
+            <div className="logo">
+                <BsFillRocketTakeoffFill size={35} color='#fff' />
+                <p className="logo-text">
+                Harry.<span>com</span>
+                </p>
+            </div>
+            <menu>
+                <ul
+                className="nav-links">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Features</a>
+                </li>
+                <li>
+                    <a href="#">Download</a>
+                </li>
+                <li>
+                    <a href="#">Subscribe</a>
+                </li>
+                <li className="nav-btn">
+                    <a className="btn btn-orange" href="#">
+                    Get Started
+                    </a>
+                </li>
+                </ul>
+            </menu>
+            <div className="menu-icons">
+                <AiOutlineBars color='#fff' size={27} />
+            </div>
+        </nav>
     );
-//   return (
-//     <nav className='container logo'>
-//       <div>
-//         <BsFillRocketTakeoffFill size={35} />
-//         <p className="logo-text">
-//             Harry.<span>com</span>
-//         </p>
-//       </div>
-//       <menu>
-//         <ul className="nav-links">
-//             <li><a href="#">Home</a></li>
-//             <li><a href="#">Features</a></li>
-//             <li><a href="#">Download</a></li>
-//             <li><a href="#">Subscribe</a></li>
-//             <li className='nav-btn'><a href="#" className='btn btn-orange'>Get Started</a></li>
-//         </ul>
-//       </menu>
-//       <div className="menu-icons">
-//         <AiOutlineBars color='#fff' size={27} />
-//       </div>
-//     </nav>
-//   )
 }
 
 export default Navbar;
